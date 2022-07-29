73901b1913f3e2f443d6a7420426f97a5ba99b98
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-07-30 08:53:52 +0900

    Change the undocumented following definitions   [Before]   our $STDIN : ro int;   our $STDOUT : ro int;   our $STDERR : ro int;   [After]   native static method STDERR : int ();   native static method STDIN : int ();   native static method STDOUT : int ();

--- lib/SPVM/IO/File.c
+++ lib/SPVM/IO/File.c
@@ -7,17 +7,36 @@
 
 static const char* FILE_NAME = "IO/File.c";
 
-int32_t SPVM__IO__File__init_constant_values(SPVM_ENV* env, SPVM_VALUE* stack) {
+int32_t SPVM__IO__File__STDERR(SPVM_ENV* env, SPVM_VALUE* stack) {
+
+#ifdef stderr
+  stack[0].ival = fileno(stderr);
+#else
+  return env->die(env, stack, "stderr is not defined in this system", FILE_NAME, __LINE__);
+#endif
   
-  int32_t e;
+  return 0;
+}
+
+int32_t SPVM__IO__File__STDIN(SPVM_ENV* env, SPVM_VALUE* stack) {
+
+#ifdef stdin
+  stack[0].ival = fileno(stdin);
+#else
+  return env->die(env, stack, "stdin is not defined in this system", FILE_NAME, __LINE__);
+#endif
   
-  env->set_class_var_int_by_name(env, stack, "IO::File", "$STDIN", fileno(stdin), &e, FILE_NAME, __LINE__);
-  if (e) { return e; }
-  env->set_class_var_int_by_name(env, stack, "IO::File", "$STDOUT", fileno(stdout), &e, FILE_NAME, __LINE__);
-  if (e) { return e; }
-  env->set_class_var_int_by_name(env, stack, "IO::File", "$STDERR", fileno(stderr), &e, FILE_NAME, __LINE__);
-  if (e) { return e; }
-    
+  return 0;
+}
+
+int32_t SPVM__IO__File__STDOUT(SPVM_ENV* env, SPVM_VALUE* stack) {
+
+#ifdef stdout
+  stack[0].ival = fileno(stdout);
+#else
+  return env->die(env, stack, "stdout is not defined in this system", FILE_NAME, __LINE__);
+#endif
+  
   return 0;
 }
 
