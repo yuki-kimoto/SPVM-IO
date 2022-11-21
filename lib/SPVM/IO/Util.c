3374629627b75bb501b852860f68c026e753b9e8
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-11-21 15:14:28 +0900

    Socket header and source code becomes resource

--- lib/SPVM/IO/Util.c
+++ lib/SPVM/IO/Util.c
@@ -1,93 +1,9 @@
-// Windows 8.1+
-#define _WIN32_WINNT 0x0603
-
 #include "spvm_native.h"
 
 #include <errno.h>
 #include <assert.h>
 
-#ifdef _WIN32
-  #include <ws2tcpip.h>
-  #include <winsock2.h>
-  #include <io.h>
-  #include <winerror.h>
-#else
-  #include <unistd.h>
-  #include <sys/types.h>
-  #include <sys/socket.h>
-  #include <netinet/in.h>
-  #include <netinet/ip.h>
-  #include <netdb.h>
-  #include <arpa/inet.h>
-#endif
-
-static int32_t FIELD_INDEX_ADDRINFO_MEMORY_ALLOCATED = 0;
-static int32_t ADDRINFO_MEMORY_ALLOCATED_BY_NEW = 1;
-static int32_t ADDRINFO_MEMORY_ALLOCATED_BY_GETADDRINFO = 2;
-
-static int32_t socket_errno (void) {
-#ifdef _WIN32
-  return WSAGetLastError();
-#else
-  return errno;
-#endif
-}
-
-#ifdef _WIN32
-static void* socket_strerror_string_win (SPVM_ENV* env, SPVM_VALUE* stack, int32_t error_number, int32_t length) {
-  char* error_message = NULL;
-  FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, 
-                 NULL, error_number,
-                 MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US),
-                 (LPSTR)&error_message, length, NULL);
-  
-  void* obj_error_message = env->new_string(env, stack, error_message, strlen(error_message));
-  
-  LocalFree(error_message);
-  
-  return obj_error_message;
-}
-#endif
-
-static void* socket_strerror_string (SPVM_ENV* env, SPVM_VALUE* stack, int32_t error_number, int32_t length) {
-  void*
-#ifdef _WIN32
-  obj_strerror_value = socket_strerror_string_win(env, stack, error_number, length);
-#else
-  obj_strerror_value = env->strerror_string(env, stack, error_number, length);
-#endif
-  return obj_strerror_value;
-}
-
-int32_t SPVM__Sys__Socket__socket_errno(SPVM_ENV* env, SPVM_VALUE* stack) {
-  int32_t ret_socket_errno = socket_errno();
-  
-  stack[0].ival = ret_socket_errno;
-  
-  return 0;
-}
-
-int32_t SPVM__Sys__Socket__socket_strerror(SPVM_ENV* env, SPVM_VALUE* stack) {
-  int32_t error_number = stack[0].ival;
-  int32_t length = stack[1].ival;
-  
-  void* obj_socket_strerror = socket_strerror_string(env, stack, error_number, length);
-  
-  stack[0].oval = obj_socket_strerror;
-  
-  return 0;
-}
-
-static const char* socket_strerror(SPVM_ENV* env, SPVM_VALUE* stack, int32_t error_number, int32_t length) {
-  void* obj_socket_strerror = socket_strerror_string(env, stack, error_number, length);
-  
-  const char* ret_socket_strerror = NULL;
-  if (obj_socket_strerror) {
-    ret_socket_strerror = env->get_chars(env, stack, obj_socket_strerror);
-  }
-  
-  return ret_socket_strerror;
-}
+#include "SPVM__IO__Util.h"
 
 static const char* FILE_NAME = "IO/Util.c";
 
