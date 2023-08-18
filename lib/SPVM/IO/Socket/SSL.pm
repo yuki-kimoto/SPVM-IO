7fd0b5040325c438b3d0d8afbe6c4abe103c29d9
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-08-19 08:32:22 +0900

    IO::Socket::SSL is added.

---
+++ lib/SPVM/IO/Socket/SSL.pm
@@ -0,0 +1,45 @@
+package SPVM::IO::Socket::SSL;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::IO::Socket::SSL - Short Description
+
+=head1 Description
+
+The IO::Socket::SSL class of L<SPVM> has methods for someting.
+
+=head1 Usage
+
+  use IO::Socket::SSL;
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+[--user-name] C<[--user-email]>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 [--user-name]
+
+MIT License
+
