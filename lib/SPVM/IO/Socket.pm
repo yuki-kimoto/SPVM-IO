b49d3419d9c5ce00cf33356b9c7d18146c4a9f6a
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-09-05 08:19:09 +0900

    The following fields and methods in the IO::Socket class are added, deleted, or changed.
      [Incompatibe Changes]
        * The following fields and methods in the IO::Socket class are added, deleted, or changed.
          -  has fd : protected ro int;
          +  has socket_fd : protected int;

--- lib/SPVM/IO/Socket.pm
+++ lib/SPVM/IO/Socket.pm
@@ -52,9 +52,9 @@ L<IO::Handle|SPVM::IO::Handle>.
 
 See also L<Sys::Socket::Sockaddr|SPVM::Sys::Socket::Sockaddr>.
 
-=head2 fd
+=head2 socket_fd
 
-  has fd : protected int;
+  has socket_fd : protected int;
 
 =head2 Listen
 
