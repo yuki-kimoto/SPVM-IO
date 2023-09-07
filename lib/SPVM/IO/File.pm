7929251a5c8ce5c0665f9a81fddfcfc897010a1a
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-09-07 15:36:35 +0900

    The following methods are moved from the IO::File class to the IO::Handle class.
      [Incompatibe Changes]
        * The following methods are moved from the IO::File class to the IO::Handle class.
    
          method sync : int ();
          method stat : Sys::IO::Stat ();
          method truncate : int ($legnth : long);

--- lib/SPVM/IO/File.pm
+++ lib/SPVM/IO/File.pm
@@ -121,20 +121,6 @@ Gets the file descriptor of L</"stream"> field.
 
   method read : int ($string : mutable string, $length : int = -1, $offset : int = 0);
 
-=head2 truncate
-
-  method truncate : int ($legnth : long);
-
-Truncates to a size of precisely length bytes.
-
-=head2 stat
-
-  method stat : Sys::IO::Stat ();
-
-=head2 sync
-
-  method sync : int ();
-
 =head1 See Also
 
 =head2 Perl's IO::File
