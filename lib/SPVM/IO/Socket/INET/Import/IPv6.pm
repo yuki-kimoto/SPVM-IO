be167fb1ec250bfe4ca090a9b4bfaddeb749bfb3
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-05-13 09:54:36 +0900

    Add IO::Socket::INET::Import::IPv4 and IO::Socket::INET::Import::IPv6.

---
+++ lib/SPVM/IO/Socket/INET/Import/IPv6.pm
@@ -0,0 +1,61 @@
+package SPVM::IO::Socket::INET::Import::IPv6;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::IO::Socket::INET::Import::IPv6 - Short Description
+
+=head1 Description
+
+The IO::Socket::INET::Import::IPv6 class in L<SPVM> has methods to do someting.
+
+=head1 Usage
+
+  use IO::Socket::INET::Import::IPv6;
+
+=head1 Details
+
+
+
+=head1 Inheritance
+
+
+
+=head1 Interfaces
+
+
+
+=head1 Enumerations
+
+
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2024 Yuki Kimoto
+
+MIT License
+
