5752891847b3852a39a200b14deb8eb1d874d373
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-05-18 07:56:38 +0900

    Improve the doc of IO::File.

--- lib/SPVM/IO/File.pm
+++ lib/SPVM/IO/File.pm
@@ -4,22 +4,29 @@ package SPVM::IO::File;
 
 =head1 Name
 
-SPVM::IO::File - File Input/Output
+SPVM::IO::File - File IO
 
 =head1 Usage
   
   use IO::File;
   
-  my $io_file = IO::File->new("foo.txt", ">");
-  $io_file->print("Hello");>
+  # Write a line
+  my $fh = IO::File->new("foo.txt", ">");
+  $fh->say("Hello");>
+  
+  # Read lines
+  my $fh = IO::File->new("foo.txt", "<");
+  while (my $line = $fh->readline) {
+    
+  }
 
 =head1 Description
 
-IO::File class has methods for File IO.
+IO::File class in L<SPVM> has methods for File IO.
 
 =head1 Super Class
 
-L<IO::Handle|SPVM::IO::Handle>.
+L<IO::Handle|SPVM::IO::Handle>
 
 =head1 Fields
 
@@ -39,19 +46,71 @@ The current line number. This value is incremented by L</"getline"> method.
 
 =head2 new
 
-C<static method new : IO::File ($file_name : string = undef, $open_mode : string = undef);>
+C<static method new : L<IO::File|SPVM::IO::File> ($file_name : string = undef, $open_mode : string = undef);>
+
+Creates a new L<IO::File|SPVM::IO::File> object.
+
+And opens a file given the file name $file_name and the open mode $open_mode by calling L</"open"> method.
+
+And returns the new object.
+
+If $file_name is not defined, a file is not opened.
+
+Exceptions:
+
+Exceptions thrown by L</"open"> method could be thrown.
 
 =head2 new_from_fd
 
 C<static method new_from_fd : IO::Handle ($fd : int, $open_mode : string);>
 
+Creates a new L<IO::File|SPVM::IO::File> object.
+
+And opens a file given the file descriptor $fd and the open mode $open_mode by calling L</"fdopen"> method.
+
+And returns the new object.
+
+Exceptions:
+
+Exceptions thrown by L</"fdopen"> method could be thrown.
+
 =head2 open
 
 C<method open : void ($file_name : string, $open_mode : string);>
 
+Opens a file given the file name $file_name and the open mode $open_mode.
+
+This method calls L<Sys#open|SPVM::Sys/"open"> method.
+
+L</"FileStream"> field is set to the opened file stream.
+
+L</"InputLineNumber"> field is set to 0.
+
+Exceptions:
+
+The file name $file_name must be defined. Otherwise an exception is thrown.
+
+The open mode $open_mode must be defined. Otherwise an exception is thrown.
+
+If a file is already opened, an exception is thrown.
+
 =head2 fdopen
 
 C<method fdopen : void ($fd : int, $open_mode : string);>
+
+Opens a file given the file descriptor $fd and the open mode $open_mode.
+
+This method calls L<Sys#fdopen|SPVM::Sys/"fdopen"> method.
+
+L</"FileStream"> field is set to the opened file stream.
+
+L</"InputLineNumber"> field is set to 0.
+
+Exceptions:
+
+The open mode $open_mode must be defined. Otherwise an exception is thrown.
+
+If a file is already opened, an exception is thrown.
 
 =head1 Instance Methods
 
@@ -59,7 +118,7 @@ C<method fdopen : void ($fd : int, $open_mode : string);>
 
 C<method input_line_number : long ();>
 
-Returns the value of L</"InputLineNumber> field.
+Returns the value of L</"InputLineNumber"> field.
 
 =head2 close
 
