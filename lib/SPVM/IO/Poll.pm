ac2a6bff40ec436f8a69fab41e38067a631e83f2
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-05-28 07:17:47 +0900

    Add some IO::Poll Implementation.

--- lib/SPVM/IO/Poll.pm
+++ lib/SPVM/IO/Poll.pm
@@ -42,6 +42,24 @@ The IO::Poll class in L<SPVM> has methods to do someting.
 
 =head1 Instance Methods
 
+  mask ( IO [, EVENT_MASK ] )
+  
+  If EVENT_MASK is given, then, if EVENT_MASK is non-zero, IO is added to the list of file descriptors and the next call to poll will check for any event specified in EVENT_MASK. If EVENT_MASK is zero then IO will be removed from the list of file descriptors.
+
+  If EVENT_MASK is not given then the return value will be the current event mask value for IO.
+
+  poll ( [ TIMEOUT ] )
+  Call the system level poll routine. If TIMEOUT is not specified then the call will block. Returns the number of handles which had events happen, or -1 on error.
+
+  events ( IO )
+  Returns the event mask which represents the events that happened on IO during the last call to poll.
+
+  remove ( IO )
+  Remove IO from the list of file descriptors for the next poll.
+
+  handles( [ EVENT_MASK ] )
+  Returns a list of handles. If EVENT_MASK is not given then a list of all handles known will be returned. If EVENT_MASK is given then a list of handles will be returned which had one of the events specified by EVENT_MASK happen during the last call ti poll  
+}
 
 
 
