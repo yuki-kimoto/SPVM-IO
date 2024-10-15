972c71741b55dc243e0379c6764bc37662ee1e9b
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-10-15 13:03:49 +0900

    Improve docs for IO::Socket::IP::Import::IPv6 and IO::Socket::IP::Import::IPv4.

--- lib/SPVM/IO/Socket/IP/Import/IPv6.pm
+++ lib/SPVM/IO/Socket/IP/Import/IPv6.pm
@@ -6,11 +6,11 @@ package SPVM::IO::Socket::IP::Import::IPv6;
 
 =head1 Name
 
-SPVM::IO::Socket::IP::Import::IPv6 - IPv6 Information
+SPVM::IO::Socket::IP::Import::IPv6 - Importable Methods for IPv6
 
 =head1 Description
 
-The IO::Socket::IP::Import::IPv6 class in L<SPVM> has methods to get IPv6 information.
+IO::Socket::IP::Import::IPv6 interface in L<SPVM> provides importable methods for IPv6.
 
 =head1 Usage
 
@@ -22,53 +22,59 @@ The IO::Socket::IP::Import::IPv6 class in L<SPVM> has methods to get IPv6 inform
 
 C<method sockname : L<Sys::Socket::Sockaddr|SPVM::Sys::Socket::Sockaddr> ();>
 
-An interface method to return the information containing the local address and port.
+Returns L<Sys::Socket::Sockaddr|SPVM::Sys::Socket::Sockaddr> object containing a local address and a local port.
 
-=head2 sockaddr
-
-C<method sockaddr : L<Sys::Socket::In_addr_base|SPVM::Sys::Socket::In_addr_base> ();>
-
-An interface method with a default implementation to return the local address.
-
-The local address is a L<Sys::Socket::In6_addr|SPVM::Sys::Socket::In6_addr> object.
-
-=head2 sockhost
-
-C<method sockhost : string ();>
-
-An interface method with a default implementation to return the local host name.
-
-=head2 sockport
-
-C<method sockport : int ();>
-
-An interface method with a default implementation to return the local port.
+This method must be implemented by the class that declares this interface.
 
 =head2 peername
 
 C<method peername : L<Sys::Socket::Sockaddr|SPVM::Sys::Socket::Sockaddr> ();>
 
-An interface method to return the information containing the remote address and port.
+Returns a L<Sys::Socket::Sockaddr|SPVM::Sys::Socket::Sockaddr> object containing a remote address and a local port.
+
+This method must be implemented by the class that declares this interface.
+
+=head1 Instance Methods
+
+=head2 sockaddr
+
+C<method sockaddr : L<Sys::Socket::In_addr_base|SPVM::Sys::Socket::In_addr_base> ();>
+
+Returns a local address.
+
+The type of the returned object is L<Sys::Socket::In6_addr|SPVM::Sys::Socket::In6_addr>.
+
+=head2 sockhost
+
+C<method sockhost : string ();>
+
+Returns a local host name.
+
+=head2 sockport
+
+C<method sockport : int ();>
+
+Returns a local port.
 
 =head2 peeraddr
 
 C<method peeraddr : L<Sys::Socket::In_addr_base|SPVM::Sys::Socket::In_addr_base> ();>
 
-An interface method with a default implementation to return the remote address.
+Returns a remote address.
 
-The remote address is a L<Sys::Socket::In6_addr|SPVM::Sys::Socket::In6_addr> object.
+The type of the returned object is L<Sys::Socket::In6_addr|SPVM::Sys::Socket::In6_addr>.
 
 =head2 peerhost
 
 C<method peerhost : string ();>
 
-An interface method with a default implementation to return the remote host name.
+Returns a remote host name.
 
 =head2 peerport
 
 C<method peerport : int ();>
 
-An interface method with a default implementation to return the remote port.
+Returns a remote port.
 
 =head1 Copyright & License
 
