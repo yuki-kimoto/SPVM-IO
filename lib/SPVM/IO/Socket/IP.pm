ab07e2b6315842673ab270651a39a0e3342de179
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-10-15 13:37:03 +0900

    IO::Socket::IP#new dose not set SOCK_RAW Type for IPPROTO_ICMP Proto. This is Perl's IO::Socket::IP compatible beheivior.
      [Incompatibe Changes]
        * IO::Socket::IP#new dose not set SOCK_RAW Type for IPPROTO_ICMP Proto. This is Perl's IO::Socket::IP compatible beheivior.

--- lib/SPVM/IO/Socket/IP.pm
+++ lib/SPVM/IO/Socket/IP.pm
@@ -167,8 +167,6 @@ If the value of C<Proto> is C<IPPROTO_TCP>, the C<Type> field is set to C<SOCK_S
 
 If the value of C<Proto> is C<IPPROTO_UDP>, the C<Type> field is set to C<SOCK_DGRAM>.
 
-If the value of C<Proto> is C<IPPROTO_ICMP>, the C<Type> field is set to C<SOCK_RAW>.
-
 =head2 sockaddr
 
 C<method sockaddr : L<Sys::Socket::In_addr_base|SPVM::Sys::Socket::In_addr_base> ();>
